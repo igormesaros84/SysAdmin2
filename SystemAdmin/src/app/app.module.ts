3dcfc4d9eaeab41a6eed4b1bc7f5b4458d77e170
Igor <igorm84@live.com>
2020-01-23 14:08:57 +0000

    Moved login to separate module

--- SystemAdmin/src/app/app.module.ts
+++ SystemAdmin/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: '', redirectTo: 'login', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
